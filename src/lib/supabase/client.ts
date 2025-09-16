5878230da571102bb09b8c23836e93be1daae598
Abaasa Albert <altechalbert01@gmail.com>
2025-09-16 14:30:05 +0300

    change in the env

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,8 +1,7 @@
-
 import { createClient } from '@supabase/supabase-js'
 
 // Create a single supabase client for interacting with your database
 export const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    'https://qhttfjwbcabtoyrlwfzp.supabase.co',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFodHRmandiY2FidG95cmx3ZnpwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc4NzE5NDAsImV4cCI6MjA3MzQ0Nzk0MH0.bzecnKMFA3cuQQnvC5E9Kbo9E77rIvydNkp-z97EGhw'
 )
